28bb9386cefce5f21e76b325eae596de15965bf0
Sam Wilson <sam@samwilson.id.au>
2017-10-20 10:24:10 +0800

    Set timeout for DB etc.

---
+++ CONTRIBUTING.md
@@ -0,0 +1,6 @@
+Contributing
+============
+
+
+We follow the [Semantic Versioning Guidelines](http://semver.org).
+When creating a new version, update the number in `src/Config.php` and tag it.
--- src/Database/Database.php
+++ src/Database/Database.php
@@ -2,12 +2,15 @@
 
 namespace App\Database;
 
-use \App\Config;
+use App\Config;
+use Exception;
+use PDO;
 use PDOException;
+use PDOStatement;
 
 class Database {
 
-	/** @var \PDO */
+	/** @var PDO */
 	static protected $pdo;
 
 	/** @var array|string */
@@ -19,10 +22,11 @@ class Database {
 		}
 		$host = Config::databaseHost();
 		$dsn = "mysql:host=$host;dbname=" . Config::databaseName() . ";charset=utf8";
-		$attr = [ \PDO::ATTR_TIMEOUT => 10 ];
-		self::$pdo = new \PDO( $dsn, Config::databaseUser(), Config::databasePassword(), $attr );
-		self::$pdo->setAttribute( \PDO::ATTR_ERRMODE, \PDO::ERRMODE_EXCEPTION );
-		$this->setFetchMode( \PDO::FETCH_OBJ );
+		$attr = [ PDO::ATTR_TIMEOUT => 10 ];
+		self::$pdo = new PDO( $dsn, Config::databaseUser(), Config::databasePassword(), $attr );
+		self::$pdo->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
+		$this->setFetchMode( PDO::FETCH_OBJ );
+		self::$pdo->exec( "SET @@session.wait_timeout = 1200" );
 	}
 
 	public static function getQueries() {
@@ -30,41 +34,42 @@ class Database {
 	}
 
 	public function setFetchMode( $fetchMode ) {
-		return self::$pdo->setAttribute( \PDO::ATTR_DEFAULT_FETCH_MODE, $fetchMode );
+		return self::$pdo->setAttribute( PDO::ATTR_DEFAULT_FETCH_MODE, $fetchMode );
 	}
 
 	/**
 	 * Get a result statement for a given query. Handles errors.
 	 *
 	 * @param string $sql The SQL statement to execute.
-	 * @param array $params Array of param => value pairs.
-	 * @return \PDOStatement Resulting PDOStatement.
+	 * @param string|bool $params Array of param => value pairs, or false if none are needed.
+	 * @return PDOStatement Resulting PDOStatement.
+     * @throws Exception If the SQL couldn't be executed.
 	 */
 	public function query( $sql, $params = false, $class = false, $classArgs = false ) {
 		if ( !empty( $class ) && !class_exists( $class ) ) {
-			throw new \Exception( "Class not found: $class" );
+			throw new Exception( "Class not found: $class" );
 		}
 		try {
 			if ( is_array( $params ) && count( $params ) > 0 ) {
 				$stmt = self::$pdo->prepare( $sql );
 				foreach ( $params as $placeholder => $value ) {
 					if ( is_bool( $value ) ) {
-						$type = \PDO::PARAM_BOOL;
+						$type = PDO::PARAM_BOOL;
 					} elseif ( is_null( $value ) ) {
-						$type = \PDO::PARAM_NULL;
+						$type = PDO::PARAM_NULL;
 					} elseif ( is_int( $value ) ) {
-						$type = \PDO::PARAM_INT;
+						$type = PDO::PARAM_INT;
 					} else {
-						$type = \PDO::PARAM_STR;
+						$type = PDO::PARAM_STR;
 					}
 					// echo '<li>';var_dump($value, $type);
 					$stmt->bindValue( $placeholder, $value, $type );
 				}
 				if ( $class ) {
-					$stmt->setFetchMode( \PDO::FETCH_CLASS | \PDO::FETCH_PROPS_LATE, $class,
+					$stmt->setFetchMode( PDO::FETCH_CLASS | PDO::FETCH_PROPS_LATE, $class,
 						$classArgs );
 				} else {
-					$stmt->setFetchMode( \PDO::FETCH_OBJ );
+					$stmt->setFetchMode( PDO::FETCH_OBJ );
 				}
 				$result = $stmt->execute();
 				if ( !$result ) {
@@ -76,7 +81,7 @@ class Database {
 			} else {
 				if ( $class ) {
 					$stmt =
-						self::$pdo->query( $sql, \PDO::FETCH_CLASS | \PDO::FETCH_PROPS_LATE, $class,
+						self::$pdo->query( $sql, PDO::FETCH_CLASS | PDO::FETCH_PROPS_LATE, $class,
 							$classArgs );
 				} else {
 					$stmt = self::$pdo->query( $sql );
@@ -86,7 +91,7 @@ class Database {
 			$msg =
 				$e->getMessage() . ' -- Unable to execute SQL: ' . ' <code>' . $sql . '</code> ' .
 				' Parameters: ' . var_export( $params, true );
-			throw new \Exception( $msg );
+			throw new Exception( $msg );
 		}
 
 		self::$queries[] = [ 'sql' => $sql, 'params' => $params ];
